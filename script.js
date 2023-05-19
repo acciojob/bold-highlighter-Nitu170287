6f2dcfddee343f73d0205f8e2078cfa33d940344
Nitu170287 <108494260+Nitu170287@users.noreply.github.com>
2023-05-19 14:42:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function highlight() {
     //Write your code here
-
+var item = document.getElementsByTagName(strong);
+	for(let i=0;i<item.length;i++){
+		item[i].style.color = "green";
+	}
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+var item = document.getElementsByTagName(strong);
+	for(let i=0;i<item.length;i++){
+		item[i].style.color = "black";
+	}
     
 }
